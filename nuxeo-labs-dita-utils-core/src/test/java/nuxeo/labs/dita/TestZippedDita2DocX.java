2cc1639a48156e4acc7628e2ad701bfe29d9499d
Josh Fletcher <jfletcher@nuxeo.com>
2016-08-24 16:07:41 -0700

    Test extraction is working.

--- nuxeo-labs-dita-utils-core/src/test/java/nuxeo/labs/dita/TestZippedDita2DocX.java
+++ nuxeo-labs-dita-utils-core/src/test/java/nuxeo/labs/dita/TestZippedDita2DocX.java
@@ -19,27 +19,15 @@
 
 package nuxeo.labs.dita;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.inject.Inject;
-
-import nuxeo.labs.dita.operations.ZippedDita2DocXOp;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.common.utils.FileUtils;
 import org.nuxeo.ecm.automation.AutomationService;
-import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.OperationException;
 import org.nuxeo.ecm.automation.test.AutomationFeature;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
@@ -47,6 +35,13 @@ import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(FeaturesRunner.class)
 @Features(AutomationFeature.class)
@@ -70,7 +65,6 @@ public class TestZippedDita2DocX {
         assertNotNull(session);
         assertNotNull(automationService);
 
-
         zipFile = FileUtils.getResourceFileFromContext(THE_ZIP);
         zipFileBlob = new FileBlob(zipFile);
 
@@ -78,7 +72,13 @@ public class TestZippedDita2DocX {
 
     @Test
     public void shouldCallTheOperation() throws OperationException {
+        final String ditaMapFileName = "dita_test.ditamap";
 
+        ZippedDita2DocX pe = new ZippedDita2DocX(zipFileBlob);
+
+        Blob result = pe.getDocx();
+
+        assertEquals(result.getFilename(), ditaMapFileName);
     }
 
 
